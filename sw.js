3e79ce2e7f129776b731663d1f2ad866407469d3
prrdg <zkarxbox@gmail.com>
2026-07-20 03:00:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "venty-v6";
+const CACHE = "venty-v7";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
